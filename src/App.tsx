8766f1a2837aefc3961c0e34ba5933f7e96e3cd6
João Alves <168280467+JoaoMarceloAlvesDev@users.noreply.github.com>
2026-07-03 11:48:20 -0300

    Fix import case for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home";
 import Jogo from "./components/Jogo";
 
 function App() {

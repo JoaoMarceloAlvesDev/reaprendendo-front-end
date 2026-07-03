19fba7c50f6e29bba36e3603804375a7b0ba5f9c
Marcos Rocha <mtrocha.dev@gmail.com>
2026-07-03 16:53:22 -0300

    URL RailWay backend

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 
-export const API_URL = import.meta.env.VITE_API_URL || 'http://reaprendendo.up.railway.app';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://reaprendendo-back-end.up.railway.app';
 
 
 export const socket = io(API_URL, {

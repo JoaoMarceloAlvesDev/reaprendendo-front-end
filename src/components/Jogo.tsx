203b25a49adba18c41abd02c2be8a696086970b9
Marcos Rocha <mtrocha.dev@gmail.com>
2026-07-03 14:34:57 -0300

    pontuacao tirada

--- src/components/Jogo.tsx
+++ src/components/Jogo.tsx
@@ -1,55 +1,64 @@
 import './Jogo.css'
 import { Comparador } from './Comparador'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Jogo() {
 
-  const texto = {
-    txt1: 'Você é rico, mas tudo que você comer tem gosto de coco.',
-    txt2: 'Você consegue achar a pessoa da sua vida, terá uma vida maravilhosa com ela, porém você nunca morrerá e nunca mais vai esquercer ela.',
-    txt3: 'opcao 3',
-    txt4: 'opcao 4',
-    txt5: 'opcao 5',
-    txt6: 'opcao 6',
-    txt7: 'opcao 7',
-    txt8: 'opcao 8',
-    txt9: 'opcao 9',
-    txt10: 'opcao 10',
-  }
+  const texto = [
+    'Você é rico, mas tudo que você comer tem gosto de coco.',
+    'Você consegue achar a pessoa da sua vida, terá uma vida maravilhosa com ela, porém você nunca morrerá e nunca mais vai esquercer ela.',
+    'opcao 3',
+    'opcao 4',
+    'opcao 5',
+    'opcao 6',
+    'opcao 7',
+    'opcao 8',
+    'opcao 9',
+    'opcao 10',
+  ]
 
-  const valoresReal = {
-    v1: 154,
-    v2: 10,
-    v3: 19,
-    v4: 53,
-    v5: 634,
-    v6: 674,
-    v7: 85,
-    v8: 13,
-    v9: 683,
-    v10: 97,
-  }
+  const valoresReal = [
+    154,
+    10,
+    19,
+    53,
+    634,
+    674,
+    85,
+    13,
+    683,
+    97,
+  ]
 
   const nome = "Nome Aqui"
-  const [pontos, setPontos] = useState(0)
   const [valor1, setValor1] = useState(0)
   const [valor2, setValor2] = useState(0)
   const [cardOneClick, setCardOneClick] = useState(true);
+  const [txt1, setTxt1] = useState(0)
+  const [txt2, setTxt2] = useState(1)
+  const [v1, setV1] = useState(0)
+  const [v2, setV2] = useState(1)
 
   function cardClick(card: number) {
     if (!cardOneClick) return;
 
-    setValor1(valoresReal.v1)
-    setValor2(valoresReal.v2)
-
-    if (valoresReal.v1 > valoresReal.v2 && card == 1) {
-      setPontos(pontos + 1)
-    }
-    if (valoresReal.v1 < valoresReal.v2 && card == 2) {
-      setPontos(pontos + 1)
-    }
+    setValor1(valoresReal[v1])
+    setValor2(valoresReal[v2])
 
     setCardOneClick(false)
+  }
+
+  function proximo() {
+    setValor1(0)
+    setValor2(0)
+
+    setTxt1(txt1 + 2)
+    setTxt2(txt2 + 2)
+    setV1(v1 + 2)
+    setV2(v2 + 2)
+
+    setCardOneClick(true)
   }
 
   return (
@@ -57,15 +66,15 @@ function Jogo() {
       <div className='container'>
         <div className="container-jogo">
           <div className="container-cabecalho">
-          <p>{nome}</p>
-          <p>{pontos}</p>
+            <Link to="/" style={{textDecoration:"none", color:"rgb(40, 126, 255)"}}>&lt;- Logout</Link>
+            <p style={{marginTop:0}}>{nome}</p>
           </div>
           <div className="cards-container">
-            <button className="card btn" id='card1' onClick={() => cardClick(1)}>{texto.txt1}</button>
-            <button className="card btn" id='card2' onClick={() => cardClick(2)}>{texto.txt2}</button>
+            <button className="card btn" id='card1' onClick={() => cardClick(1)}>{texto[txt1]}</button>
+            <button className="card btn" id='card2' onClick={() => cardClick(2)}>{texto[txt2]}</button>
           </div>
           <Comparador valor1={valor1} valor2={valor2} />
-          <button className='btn' style={{marginTop:"20px", width: "100%"}}>Próximo</button>
+          <button className='btn' style={{ marginTop: "20px", width: "100%" }} onClick={proximo}>Próximo</button>
         </div>
       </div>
     </>

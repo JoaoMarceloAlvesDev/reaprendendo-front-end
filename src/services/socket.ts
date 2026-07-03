4588e8cc982a6f38c917a2970c4678f4f0f2493d
Marcos Rocha <mtrocha.dev@gmail.com>
2026-07-03 17:01:29 -0300

    socket update

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,10 +1,8 @@
 
-import { io } from 'socket.io-client';
+
 
 
 export const API_URL = import.meta.env.VITE_API_URL || 'http://reaprendendo-back-end.up.railway.app';
 
-
-export const socket = io(API_URL, {
-  autoConnect: true,
-});
+import { io } from 'socket.io-client';
+export const socket = io(API_URL);

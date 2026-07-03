e484a5f72417a45654882fc2be3acfbf1a02d06f
Marcos Rocha <mtrocha.dev@gmail.com>
2026-07-03 13:59:11 -0300

    inicio jogo

--- src/components/Comparador.tsx
+++ src/components/Comparador.tsx
@@ -12,16 +12,16 @@ export function Comparador({ valor1, valor2 }: Props) {
     const valor2Percent = total ? (valor2 / total) * 100 : 0;
 
     return (
-        <div style={{ width: "100%", display: "flex", flexDirection: "row", gap: 20, justifyContent: "center", alignItems: "center", marginTop: 20}}>
-            <p style={{color: "rgb(40, 126, 255)",width: "200px", textAlign: "start", fontSize: "20px"}}>{valor1 > 0 && `${valor1Percent.toFixed(0)}% (${valor1})`}</p>
-            <div style={{ display: "flex", height: 36, borderRadius: 8, overflow: "hidden", background: "#eee", width: "100%", }}>
+        <div style={{width: "100%", display: "flex", flexDirection: "row", gap: 20, justifyContent: "center", alignItems: "center", marginTop: 20}}>
+            <p style={{position: "absolute" ,height: 40, color: "#ffffff",width: "200px", textAlign: "start", fontSize: "20px", marginTop: 35, left: 0}}>{valor1 > 0 && `${valor1Percent.toFixed(0)}% (${valor1})`}</p>
+            <div style={{ display: "flex", height: 40, borderRadius: 8, overflow: "hidden", background: "#eee", width: "100%", }}>
 
                 <div style={{ width: `${valor1Percent}%`, background: "rgb(40, 126, 255)", display: "flex", alignItems: "center", justifyContent: "center", color: "#fff", transition: "0.3s", }}></div>
 
                 <div style={{ width: `${valor2Percent}%`, background: "rgb(255, 70, 70)", display: "flex", alignItems: "center", justifyContent: "center", color: "#000", transition: "0.3s", }}></div>
 
             </div>
-            <p style={{color: "",width: "200px", textAlign: "end", fontSize: "20px"}}>{valor2 > 0 && `(${valor2}) ${valor2Percent.toFixed(0)}%`}</p>
+            <p style={{position: "absolute", height: 40, color: "#ffffff",width: "200px", textAlign: "end", fontSize: "20px", marginTop: 35}}>{valor2 > 0 && `(${valor2}) ${valor2Percent.toFixed(0)}%`}</p>
         </div>
     );
 }
--- src/components/Jogo.tsx
+++ src/components/Jogo.tsx
@@ -1,28 +1,71 @@
 import './Jogo.css'
 import { Comparador } from './Comparador'
+import { useState } from 'react'
 
 function Jogo() {
 
   const texto = {
-    txt1: 'Você é rico, mas tudo que você comer ter gosto de coco.',
-    txt2: 'Você consegue achar a pessoa da sua vida, terá uma vida maravilhosa com ela, porém você nunca morrerá e nunca mais vai esquercer ela.'
+    txt1: 'Você é rico, mas tudo que você comer tem gosto de coco.',
+    txt2: 'Você consegue achar a pessoa da sua vida, terá uma vida maravilhosa com ela, porém você nunca morrerá e nunca mais vai esquercer ela.',
+    txt3: 'opcao 3',
+    txt4: 'opcao 4',
+    txt5: 'opcao 5',
+    txt6: 'opcao 6',
+    txt7: 'opcao 7',
+    txt8: 'opcao 8',
+    txt9: 'opcao 9',
+    txt10: 'opcao 10',
   }
 
-  const valores = {
+  const valoresReal = {
     v1: 154,
-    v2: 410
+    v2: 10,
+    v3: 19,
+    v4: 53,
+    v5: 634,
+    v6: 674,
+    v7: 85,
+    v8: 13,
+    v9: 683,
+    v10: 97,
+  }
+
+  const nome = "Nome Aqui"
+  const [pontos, setPontos] = useState(0)
+  const [valor1, setValor1] = useState(0)
+  const [valor2, setValor2] = useState(0)
+  const [cardOneClick, setCardOneClick] = useState(true);
+
+  function cardClick(card: number) {
+    if (!cardOneClick) return;
+
+    setValor1(valoresReal.v1)
+    setValor2(valoresReal.v2)
+
+    if (valoresReal.v1 > valoresReal.v2 && card == 1) {
+      setPontos(pontos + 1)
+    }
+    if (valoresReal.v1 < valoresReal.v2 && card == 2) {
+      setPontos(pontos + 1)
+    }
+
+    setCardOneClick(false)
   }
 
   return (
     <>
       <div className='container'>
         <div className="container-jogo">
-
-          <div className="cards-container">
-            <button className="card btn" id='card1'>{texto.txt1}</button>
-            <button className="card btn" id='card2'>{texto.txt2}</button>
+          <div className="container-cabecalho">
+          <p>{nome}</p>
+          <p>{pontos}</p>
           </div>
-          <Comparador valor1={valores.v1} valor2={valores.v2} />
+          <div className="cards-container">
+            <button className="card btn" id='card1' onClick={() => cardClick(1)}>{texto.txt1}</button>
+            <button className="card btn" id='card2' onClick={() => cardClick(2)}>{texto.txt2}</button>
+          </div>
+          <Comparador valor1={valor1} valor2={valor2} />
+          <button className='btn' style={{marginTop:"20px", width: "100%"}}>Próximo</button>
         </div>
       </div>
     </>

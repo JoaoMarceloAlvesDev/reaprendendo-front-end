185adf7bce0924f1bcbe819b3562bae514a85a49
Marcos Rocha <mtrocha.dev@gmail.com>
2026-07-03 17:29:27 -0300

    -socket-corrigido

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,7 +2,7 @@
 
 
 
-export const API_URL = import.meta.env.VITE_API_URL || 'http://reaprendendo-back-end.up.railway.app';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://reaprendendo-back-end.up.railway.app';
 
 import { io } from 'socket.io-client';
 export const socket = io(API_URL);
